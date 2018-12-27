070dbdeeee7cf6624a8c089be5d292f3c8c35cf2
eugejoh <ejoh90@gmail.com>
2018-12-27 12:46:54 -0500

    exp format

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 rmarkdown::render_site()
 
 # render a single file only
-rmarkdown::render_site("index.Rmd")
+rmarkdown::render_site("cv.Rmd")
 
 # list which files will be removed
 rmarkdown::clean_site(preview = TRUE)
